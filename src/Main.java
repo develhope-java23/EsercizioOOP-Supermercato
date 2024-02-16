2e5f8aa62e9450d4d7e20ba5dc493b7521ca51e2
Calyen93 <genco.claudio3@gmail.com>
2024-02-16 15:03:47 +0100

    Extract Feature

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ public class Main {
 
         Product prodotto1 = new Product("Asparago", 3, 0.5f);
         System.out.println(prodotto1.getTotalPrice());
+
+        Product prodotto2 = prodotto.extract(2);
+        System.out.println(prodotto.getTotalPrice());
+        System.out.println(prodotto2.getTotalPrice());
     }
 }
