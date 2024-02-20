1fcf70bda35ab483ed11645cc5b53de9a504cf90
Calyen93 <genco.claudio3@gmail.com>
2024-02-20 16:12:38 +0100

    Fixed DiscountedProduct

--- src/DiscountedProduct.java
+++ src/DiscountedProduct.java
@@ -1,14 +1,14 @@
 public class DiscountedProduct extends Product {
     private float discount;
 
-    public DiscountedProduct(String name, int quantity, int price) {
+    public DiscountedProduct(String name, int quantity, float price, float discount) {
         super(name, quantity, price);
         this.discount = discount;
     }
 
     public float getTotalPrice() {
         float originalPrice = super.getTotalPrice();
-        float discountedPrice = originalPrice * (1 - discount);
+        float discountedPrice = originalPrice * (1 - discount / 100);
         return discountedPrice;
     }
 }
